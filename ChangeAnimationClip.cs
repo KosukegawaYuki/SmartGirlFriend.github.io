5213ba490b4ffaa77ea78e9865d3807a669e871e
kosuke <kosukegawa0820@gmail.com>
2018-12-21 11:56:32 +0900

    1221

---
+++ ChangeAnimationClip.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeAnimationClip : MonoBehaviour {
+
+	[SerializeField] AnimationClip clip;
+
+	string overrideClipName = "Clip"; //上書きするアニメーション
+
+	private AnimatorOverrideController overrideController;
+	private Animator anim;
+
+	// Use this for initialization
+	void Start () {
+		anim = GetComponent<Animator> (); 
+		overrideController = new AnimatorOverrideController ();
+		overrideController.runtimeAnimatorController = anim.runtimeAnimatorController;
+		anim.runtimeAnimatorController = overrideController;
+	}
+
+	public void ChangeClip(AnimationClip clip){
+		// ステートをキャッシュ
+		AnimatorStateInfo[] layerInfo = new AnimatorStateInfo[anim.layerCount];
+		for (int i = 0; i < anim.layerCount; i++) {
+			layerInfo [i] = anim.GetCurrentAnimatorStateInfo (i);
+		}
+
+		// AnimationClipを差し替えて、強制的にアップデート
+		// ステートがリセットされる
+		overrideController [overrideClipName] = clip;
+		anim.Update (0.0f);
+
+		// ステートを戻す
+		for (int i = 0; i < anim.layerCount; i++) {
+			anim.Play (layerInfo [i].nameHash, i, layerInfo [i].normalizedTime);
+		}
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ ChangeColorName.cs
@@ -0,0 +1,73 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using UnityEngine;
+using MiniJSON;
+
+public class ChangeColorName : MonoBehaviour{
+
+	private Color smr;
+	private SkinnedMeshRenderer Mesh0;
+	private Color color;
+	private Color colors;
+	private int custom;
+
+	void Start(){
+	}
+
+	//髪色変更
+	void ChangeColor(string custom_color){
+
+		//変更例
+		// string custom_color = "1,yellow";
+
+		//★★★色変更のコード完成
+		print("start ready[色変更]");
+
+		//引数を分割
+		string[] a = Regex.Split(custom_color,",");
+
+		//変更部位
+		custom = int.Parse(a[0]);
+		print("set custom"+custom);
+
+		//変更色
+		ColorUtility.TryParseHtmlString(a[1].ToString(), out colors);
+		print("set color"+colors);
+
+		Mesh0 = GetComponent<SkinnedMeshRenderer>();
+
+		//3Dモデルのマテリアル取得
+		Material[] mats = Mesh0.materials;
+
+		//変更クラス
+		SetMatColor(mats[custom],colors);
+	}
+
+	//色変更クラス
+	void SetMatColor(Material mesh,Color col ){
+		mesh.color = col;
+		
+		//拡散色
+		// mesh.SetColor("_Color",col);
+		// //環境色
+		// mesh.SetColor("_AmbColor",col);
+		// //反射色
+		// mesh.SetColor("_SpecularColor",col);
+	}
+
+	void Update(){
+	}
+}
+/* mats[0]---ネクタイ
+   mats[1]---髪
+   mats[2]---肌
+   mats[3]---ソックスと腕の袖周辺
+   mats[4]---トップス
+   mats[5]---トップスの下のギザギザ部分
+   mats[6]---眼球
+   mats[7]---ネクタイピンとヘッドホン
+   mats[11]---ヘッドホン
+   mats[16]---眉毛とギザギザの下側
+ */
